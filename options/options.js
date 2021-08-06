1ce98e05ebe297ee8a5f26f590fa1149ff0065eb
Crowdbotics <team@crowdbotics.com>
2021-08-06 23:25:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "coach_gigi_29587Identifier",
+  url: "https://coach_gigi_29587Identifier.botics.co",
+  api: "https://coach_gigi_29587Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
